3ea885590fabc33216d07d782de1533c370853e4
alejandro.nieto <alejandro.nieto@madisonmk.com>
2024-08-27 10:40:06 +0000

    Deleted some toString implementations for the moment.

--- src/main/java/com/digitelts/dome/trust/registry/model/IssuerDetails.java
+++ src/main/java/com/digitelts/dome/trust/registry/model/IssuerDetails.java
@@ -67,16 +67,5 @@ public class IssuerDetails {
   public int hashCode() {
     return Objects.hash(did, attributes);
   }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
 
