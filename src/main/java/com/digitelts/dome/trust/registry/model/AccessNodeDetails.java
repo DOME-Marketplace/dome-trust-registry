f6829e6762d002b962fc2affaa2332a44ff9421a
alejandro.nieto <alejandro.nieto@madisonmk.com>
2024-09-23 13:12:21 +0000

    Added name and url for the access node details.

--- src/main/java/com/digitelts/dome/trust/registry/model/AccessNodeDetails.java
+++ src/main/java/com/digitelts/dome/trust/registry/model/AccessNodeDetails.java
@@ -11,14 +11,30 @@ import java.time.LocalDateTime;
 public class AccessNodeDetails {
 
     @NotNull
+    private String name; 
+    @NotNull
     private String did; 
     @NotNull
-    private String publicKey; 
+    private String dlt_public_key; 
+    @NotNull
+    private String url; 
     @NotNull
     private LocalDateTime validFrom; 
     @NotNull
     private LocalDateTime validTo;
 
+    @Schema(name = "name", description = "The name of the access node.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
     @Schema(name = "did", description = "The Decentralized Identifier (DID) of the access node.", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonProperty("did")
     public String getDid() {
@@ -31,13 +47,26 @@ public class AccessNodeDetails {
 
     @Schema(name = "publicKey", description = "The public key associated with the DID.", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonProperty("ethereumPublicKey")
-    public String getPublicKey() {
-        return publicKey;
+    public String getDlt_public_key() {
+        return dlt_public_key;
     }
 
-    public void setPublicKey(String publicKey) {
-        this.publicKey = publicKey;
+    public void setDlt_public_key(String publicKey) {
+        this.dlt_public_key = publicKey;
     }
+
+    //getter and setter for the url property
+    @Schema(name = "url", description = "The URL of the access node.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
 
     @Schema(name = "validFrom", description = "The date from which the access node is valid in ISO 8601", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonProperty("validFrom")
@@ -45,9 +74,11 @@ public class AccessNodeDetails {
         return validFrom;
     }
 
+
     public void setValidFrom(LocalDateTime validFrom) {
         this.validFrom = validFrom;
     }
+
 
     @Schema(name = "validTo", description = "The date to which the access node is valid in ISO 8601", requiredMode = Schema.RequiredMode.REQUIRED)
     @JsonProperty("validTo")
@@ -55,9 +86,11 @@ public class AccessNodeDetails {
         return validTo;
     }
 
+
     public void setValidTo(LocalDateTime validTo) {
         this.validTo = validTo;
     }
+
 
     @Override
     public boolean equals(Object o) {
@@ -65,21 +98,23 @@ public class AccessNodeDetails {
         if (!(o instanceof AccessNodeDetails)) return false;
         AccessNodeDetails that = (AccessNodeDetails) o;
         return Objects.equals(did, that.did) &&
-                Objects.equals(publicKey, that.publicKey) &&
+                Objects.equals(dlt_public_key, that.dlt_public_key) &&
                 Objects.equals(validFrom, that.validFrom) &&
                 Objects.equals(validTo, that.validTo);
     }
 
+
     @Override
     public int hashCode() {
-        return Objects.hash(did, publicKey, validFrom, validTo);
+        return Objects.hash(did, dlt_public_key, validFrom, validTo);
     }
+
 
     @Override
     public String toString() {
         return "AccessNodeDetails{" +
                 "did='" + did + '\'' +
-                ", ethereumPublicKey='" + publicKey + '\'' +
+                ", ethereumPublicKey='" + dlt_public_key + '\'' +
                 ", validFrom=" + validFrom +
                 ", validTo=" + validTo +
                 '}';
